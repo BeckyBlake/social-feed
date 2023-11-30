72ea364e5b404defbc0cbca602a9f80b51ac60a8
Becky Blake <ryblake2@illinois.edu>
2023-11-29 23:22:45 -0600

    display posts on home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
+import Message from "@/components/message";
+import { useEffect, useState } from "react";
+import { db } from "@/utils/firebase";
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+
 export default function Home() {
+  // create a state with all the posts
+  const [allPosts, setAllPosts] = useState([]);
+
+  // get all posts
+  const getPosts = async () => {
+    const collectionRef = collection(db, "posts");
+    const q = query(collectionRef, orderBy("timestamp", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setAllPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+    return unsubscribe;
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
-    <div>
-      <main></main>
+    <div className="my-12 text-lg font-medium">
+      <h2>See what other people are saying</h2>
+      {/* <Message /> */}
+      {allPosts.map((post) => (
+        <Message {...post} key={post}></Message>
+      ))}
     </div>
   );
 }
+// 1:37:59
